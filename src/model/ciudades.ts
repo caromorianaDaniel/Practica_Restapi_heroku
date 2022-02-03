acb9eba8d692edebd5bcef73a5149f7810b0d6b7
CaroMorianaDaniel <91050871+caromorianaDaniel@users.noreply.github.com>
2022-02-03 16:54:51 +0100

    Create ciudades.ts

---
+++ src/model/ciudades.ts
@@ -0,0 +1,12 @@
+export class Ciudades{
+  private _nombre: string
+  private _preciom: number
+  private _codpost: number
+
+constructor(nombre: string,preciom: number,codpost: number){
+  this._nombre = nombre
+  this._preciom = preciom
+  this._codpost = codpost
+}
+
+}
